fbeebeabc730c7b4373555d0cc0739eefcc046bf
Siddharth <siddharth1360.be21@chitkara.edu.in>
2026-02-23 22:17:05 +0530

    Implemented S3 service and controller for generating presigned uploadUrl to upload files to S3 Bucket

--- service/src/main/java/com/infra/service/controller/S3Controller.java
+++ service/src/main/java/com/infra/service/controller/S3Controller.java
@@ -1,4 +1,23 @@
 package com.infra.service.controller;
 
+import com.infra.service.service.S3Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/s3")
 public class S3Controller {
+    private final S3Service s3Service;
+
+    public S3Controller(S3Service s3Service){
+        this.s3Service = s3Service;
+    }
+
+    @PostMapping("/upload-url")
+    public ResponseEntity<?> generateUrl(@RequestParam String fileName){
+        return ResponseEntity.ok(s3Service.generateUploadURL(fileName));
+    }
 }
--- service/src/main/java/com/infra/service/service/S3Service.java
+++ service/src/main/java/com/infra/service/service/S3Service.java
@@ -1,4 +1,50 @@
 package com.infra.service.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
 public class S3Service {
+    @Value("${aws.bucketName}")
+    private String bucketName;
+
+    private final S3Presigner s3Presigner;
+
+    public S3Service(S3Presigner s3Presigner){
+        this.s3Presigner = s3Presigner;
+    }
+
+    public Map<String,String> generateUploadURL(String fileName){
+        String key = UUID.randomUUID() + "-" + fileName;
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType("image/jpeg")
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(objectRequest)
+                .build();
+
+        PresignedPutObjectRequest presigned =
+                s3Presigner.presignPutObject(presignRequest);
+
+        return Map.of(
+                "uploadUrl", presigned.url().toString(),
+                "key",key
+        );
+    }
+
+
+
 }
